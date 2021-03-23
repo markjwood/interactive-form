b6257b1ef07aba8d260850836c12f23913a5ce3e
markjwood <mark@markjwood.com>
2021-03-23 10:01:22 -0700

    Disables activities with overlapping times. Now it is showing name & email as invalid when they're not.

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ const shirtDesign = document.getElementById("design");
 const shirtColor = document.getElementById("color");
 const shirtColorOptions = shirtColor.children;
 const activities = document.getElementById("activities");
+const activitiesInputs = activities.querySelectorAll('input');
 const totalCostP = document.getElementById("activities-cost");
 const payment = document.getElementById("payment");
 const creditCard = document.getElementById("credit-card");
@@ -69,9 +70,41 @@ shirtDesign.addEventListener('change', e => {
 // "Register for Activities" section
 
 let totalCost = 0;
+let committedTimes = [];
+
+  // Select "Main Conference" by default and disable
+// let totalCost = 200;
+// const mainConf = activities.querySelector("input[name='all']");
+// mainConf.checked = true;
+// mainConf.disabled = true;
+// totalCostP.innerHTML = `Total: $${totalCost}`;
+
+  // Function to check for overlapping activities
+
+const activityCheck = activity => {
+  const time = activity.getAttribute('data-day-and-time');
+  if (activity.checked) {
+    for (let i = 0; i < activitiesInputs.length; i++) {
+      if (activitiesInputs[i].getAttribute('data-day-and-time') === time && !activitiesInputs[i].checked) {
+        activitiesInputs[i].disabled = true;
+        activitiesInputs[i].parentElement.style.backgroundColor = '#f1f1f1';
+      }
+    }
+  } else {
+    for (let i = 0; i < activitiesInputs.length; i++) {
+      if (activitiesInputs[i].getAttribute('data-day-and-time') === time && !activitiesInputs[i].checked) {
+        activitiesInputs[i].disabled = false;
+        activitiesInputs[i].parentElement.style.backgroundColor = '';
+      }
+    }
+  }
+}
 
 activities.addEventListener("change", e => {
   const activityCost = parseInt(e.target.getAttribute("data-cost"));
+
+  activityCheck(e.target);
+
   e.target.checked ? totalCost += activityCost : totalCost -= activityCost;
 
   totalCostP.innerHTML = `Total: $${totalCost}`;
@@ -110,20 +143,52 @@ payment.addEventListener("change", e => {
 });
 
 // Form Validation
+  // test functions
+
+let testFuncs = {
+  emailInput: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(emailInput.value),
+  nameInput: /^[a-z ,.'-]{2,}$/i.test(nameInput.value)
+}
+  // validation functions 
+
+const checker = e => {
+  const hint = eval(e).nextElementSibling;
+  console.log(testFuncs);
+  if (payment.value === 'credit-card') {
+    testFuncs.cardNumber = /^[0-9]{13,16}$/.test(cardNumber.value);
+    testFuncs.zip = /^\d{5}$/.test(zip.value);
+    testFuncs.cvv = /^\d{3}$/.test(cvv.value);
+    testFuncs.expMonth = !isNaN(parseInt(expMonth.value));
+    testFuncs.expYear = !isNaN(parseInt(expYear.value));
+  } else {
+    delete testFuncs.expMonth;
+    delete testFuncs.expYear;
+    delete testFuncs.zip;
+    delete testFuncs.cvv;
+  }
+  
+  if (testFuncs[e] === false) {
+    if (hint) {
+      hint.style.display = 'block';
+    }
+    eval(e).parentElement.classList.add('not-valid');
+    eval(e).parentElement.classList.remove('valid');
+  } else {
+    if (hint) {
+      hint.style.display = 'none';
+    }
+    eval(e).parentElement.classList.add('valid');
+    eval(e).parentElement.classList.remove('not-valid');
+  }
+}
+
 
   // submit event listener
 
 formElement.addEventListener("submit", (e) => {
   // e.preventDefault();
 
-    // test functions
-  let testFuncs = {
-    emailInput: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(emailInput.value),
-    nameInput: /^[a-z ,.'-]{2,}$/i.test(nameInput.value)
-  }
-
   if (payment.value === 'credit-card') {
-    // additional tests for credit card payment only
     testFuncs.cardNumber = /^[0-9]{13,16}$/.test(cardNumber.value);
     testFuncs.zip = /^\d{5}$/.test(zip.value);
     testFuncs.cvv = /^\d{3}$/.test(cvv.value);
@@ -136,38 +201,69 @@ formElement.addEventListener("submit", (e) => {
     delete testFuncs.cvv;
   }
 
-    // run test functions
   for (const test in testFuncs) {
-    const hint = eval(test).nextElementSibling;
-    if (!testFuncs[test]) {
-      // invalid
+    if (testFuncs[test] === false) {
       e.preventDefault();
-      console.log(`${test}: ${testFuncs[test]}`);
-      console.log(eval(test).parentElement);
-      if (hint) {
-        // display hint if it exists
-        hint.style.display = 'block';
-      }
-      eval(test).parentElement.classList.add('not-valid');
-      eval(test).parentElement.classList.remove('valid');
-    } else {
-      if (hint) {
-        hint.style.display = 'none';
-      }
-      // eval(test).parentElement.classList.add('valid');
-      eval(test).parentElement.classList.remove('not-valid');
-    } 
+    }
+    checker(test);
   }
 
-  if (totalCost === 0) {
-    e.preventDefault();
-    console.log('No activities have been selected');
-    activities.classList.add('not-valid');
-    activities.classList.remove('valid');
-  } else {
-    activities.classList.add('valid');
-    activities.classList.remove('not-valid');
-  }
+// disabled
+    // test functions
+  // let testFuncs = {
+  //   emailInput: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(emailInput.value),
+  //   nameInput: /^[a-z ,.'-]{2,}$/i.test(nameInput.value)
+  // }
+
+  // if (payment.value === 'credit-card') {
+  //   // additional tests for credit card payment only
+  //   testFuncs.cardNumber = /^[0-9]{13,16}$/.test(cardNumber.value);
+  //   testFuncs.zip = /^\d{5}$/.test(zip.value);
+  //   testFuncs.cvv = /^\d{3}$/.test(cvv.value);
+  //   testFuncs.expMonth = !isNaN(parseInt(expMonth.value));
+  //   testFuncs.expYear = !isNaN(parseInt(expYear.value));
+  // } else {
+  //   delete testFuncs.expMonth;
+  //   delete testFuncs.expYear;
+  //   delete testFuncs.zip;
+  //   delete testFuncs.cvv;
+  // }
+
+  //   // run test functions
+  // for (const test in testFuncs) {
+  //   const hint = eval(test).nextElementSibling;
+  //   if (!testFuncs[test]) {
+  //     // invalid
+  //     e.preventDefault();
+  //     console.log(`${test}: ${testFuncs[test]}`);
+  //     console.log(eval(test).parentElement);
+  //     if (hint) {
+  //       // display hint if it exists
+  //       hint.style.display = 'block';
+  //     }
+  //     eval(test).parentElement.classList.add('not-valid');
+  //     eval(test).parentElement.classList.remove('valid');
+  //   } else {
+  //     if (hint) {
+  //       hint.style.display = 'none';
+  //     }
+  //     // eval(test).parentElement.classList.add('valid');
+  //     eval(test).parentElement.classList.remove('not-valid');
+  //   } 
+  // }
+
+  // if (totalCost === 0) {
+  //   e.preventDefault();
+  //   console.log('No activities have been selected');
+  //   activities.classList.add('not-valid');
+  //   activities.classList.remove('valid');
+  // } else {
+  //   activities.classList.add('valid');
+  //   activities.classList.remove('not-valid');
+  // }
+
+// end disabled
+
 });
 
 // Accessibility
